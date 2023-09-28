3e72f3d0a8f4d3380a59c24b0147bfb0545c3b1b
harshitaHD <harshitadhurwe@gmail.com>
2023-09-28 18:34:54 +0530

    another commit

--- server/index.js
+++ server/index.js
@@ -15,7 +15,12 @@ app.use(
   })
 );
 app.use(express.json());
+
 app.use("/", noteRoutes);
+
+// app.get("/", (req, res) => {
+//   res.send("Hello, World!");
+// });
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
